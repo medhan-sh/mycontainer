debe282f509fdf4bd187e25e9b86a82e7eb33800
medhan-sh <medshon@proton.me>
2026-06-03 02:59:04 +0530

    run: executes a program and creates a new hostname

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
-package main 
-import "fmt"
-func main(){
-fmt.Println("hello git")
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("bad command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
+
 }
